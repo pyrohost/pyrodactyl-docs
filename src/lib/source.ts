3d3a29045f080b5cd948fb2feb755b98b869f0fa
Ryder <719coolboy@gmail.com>
2025-11-04 01:46:41 -0700

    Fixes Source Lib code to be more maintainable & added Icons for Elytra and Blueprint

--- src/lib/source.ts
+++ src/lib/source.ts
@@ -3,21 +3,29 @@ import { loader } from 'fumadocs-core/source';
 import { createElement } from 'react';
 import { icons } from 'lucide-react';
 import PyrodactylLogo from '@/components/ui/PyrodactylLogo';
+import BlueprintLogo from '@/components/ui/BlueprintLogo';
+import { Bird } from "lucide-react"
 
 // `loader()` also assign a URL to your pages
 // See https://fumadocs.vercel.app/docs/headless/source-api for more info
 export const source = loader({
-  baseUrl: '/docs',
-  source: docs.toFumadocsSource(),
-  icon(icon) {
-    if (!icon) {
-      return
-    }
-    if (icon == "pyrodactyl") {
-      return PyrodactylLogo();
-    }
+    baseUrl: '/docs',
+    source: docs.toFumadocsSource(),
+    icon(icon) {
+        if (!icon) {
+            return
+        }
+        switch (icon) {
+            case 'pyrodactyl':
+                return PyrodactylLogo();
+            case 'blueprint':
+                return BlueprintLogo();
+            case 'elytra':
+                // WARNING: Temp fix until we have a proper icon for elytra
+                return createElement(Bird);
+        }
 
-    if (icon in icons) return createElement(icons[icon as keyof typeof icons]);
-  },
+        if (icon in icons) return createElement(icons[icon as keyof typeof icons]);
+    },
 });
 
