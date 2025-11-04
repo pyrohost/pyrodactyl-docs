075557e45a0ecb36fdf7679bbd6601f243bc8482
Ryder <719coolboy@gmail.com>
2025-11-04 15:31:03 -0700

    Adds Elytra Icon

--- src/lib/source.ts
+++ src/lib/source.ts
@@ -5,6 +5,7 @@ import { icons } from 'lucide-react';
 import PyrodactylLogo from '@/components/ui/PyrodactylLogo';
 import BlueprintLogo from '@/components/ui/BlueprintLogo';
 import { Bird } from "lucide-react"
+import ElytraIcon from '@/components/ui/ElytraIcon';
 
 // `loader()` also assign a URL to your pages
 // See https://fumadocs.vercel.app/docs/headless/source-api for more info
@@ -22,7 +23,7 @@ export const source = loader({
                 return BlueprintLogo();
             case 'elytra':
                 // WARNING: Temp fix until we have a proper icon for elytra
-                return createElement(Bird);
+                return ElytraIcon();
         }
 
         if (icon in icons) return createElement(icons[icon as keyof typeof icons]);
