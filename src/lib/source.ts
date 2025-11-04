0cfec1045d4bcecb8d86881becfa8db1aa922177
Ryder <719coolboy@gmail.com>
2025-11-04 15:39:48 -0700

    Removes Lucide Specific Bird Import

--- src/lib/source.ts
+++ src/lib/source.ts
@@ -5,7 +5,6 @@ import { icons } from 'lucide-react';
 import PyrodactylLogo from '@/components/ui/PyrodactylLogo';
 import BlueprintLogo from '@/components/ui/BlueprintLogo';
 import ElytraLogo from '@/components/ui/ElytraLogo';
-import { Bird } from "lucide-react"
 
 // `loader()` also assign a URL to your pages
 // See https://fumadocs.vercel.app/docs/headless/source-api for more info
