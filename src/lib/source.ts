25e4ecf616a413f6b817c143de8dac48188bd06a
Ryder <719coolboy@gmail.com>
2025-11-04 15:39:07 -0700

    Removes leftover code.

--- src/lib/source.ts
+++ src/lib/source.ts
@@ -6,7 +6,6 @@ import PyrodactylLogo from '@/components/ui/PyrodactylLogo';
 import BlueprintLogo from '@/components/ui/BlueprintLogo';
 import ElytraLogo from '@/components/ui/ElytraLogo';
 import { Bird } from "lucide-react"
-import ElytraIcon from '@/components/ui/ElytraIcon';
 
 // `loader()` also assign a URL to your pages
 // See https://fumadocs.vercel.app/docs/headless/source-api for more info
