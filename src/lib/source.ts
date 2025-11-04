dfda68a433d9f7e9afb12822e37e156c57466e5a
naterfute <naterfute@pyro.host>
2025-11-04 13:18:08 -0800

    add elytra logo

--- src/lib/source.ts
+++ src/lib/source.ts
@@ -4,28 +4,28 @@ import { createElement } from 'react';
 import { icons } from 'lucide-react';
 import PyrodactylLogo from '@/components/ui/PyrodactylLogo';
 import BlueprintLogo from '@/components/ui/BlueprintLogo';
+import ElytraLogo from '@/components/ui/ElytraLogo';
 import { Bird } from "lucide-react"
 
 // `loader()` also assign a URL to your pages
 // See https://fumadocs.vercel.app/docs/headless/source-api for more info
 export const source = loader({
-    baseUrl: '/docs',
-    source: docs.toFumadocsSource(),
-    icon(icon) {
-        if (!icon) {
-            return
-        }
-        switch (icon) {
-            case 'pyrodactyl':
-                return PyrodactylLogo();
-            case 'blueprint':
-                return BlueprintLogo();
-            case 'elytra':
-                // WARNING: Temp fix until we have a proper icon for elytra
-                return createElement(Bird);
-        }
+  baseUrl: '/docs',
+  source: docs.toFumadocsSource(),
+  icon(icon) {
+    if (!icon) {
+      return
+    }
+    switch (icon) {
+      case 'pyrodactyl':
+        return PyrodactylLogo();
+      case 'blueprint':
+        return BlueprintLogo();
+      case 'elytra':
+        return ElytraLogo()
+    }
 
-        if (icon in icons) return createElement(icons[icon as keyof typeof icons]);
-    },
+    if (icon in icons) return createElement(icons[icon as keyof typeof icons]);
+  },
 });
 
